d879ecb6618e19bb482b6d0123880c9b64cd0b30
Adriano Bianchi Crosoletto <adrianobianchi@outlook.com>
2023-07-10 18:52:10 -0300

    Refactoring our Invalid Login test

--- pages/login_page.py
+++ pages/login_page.py
@@ -9,8 +9,12 @@ class LoginPage(BasePage):
         self.username_field = (By.ID,"user-name")
         self.password_field = (By.ID,"password")
         self.login_button = (By.ID,"login-button")
+        self.error_login = (By.XPATH,"//*[@data-test='error']")
 
     def todo_login(self,user, password):
         self.to_write(self.username_field,user)
         self.to_write(self.password_field,password)
         self.click(self.login_button)
+
+    def verify_message_error_login(self):
+        self.check_element_exists(self.error_login)
