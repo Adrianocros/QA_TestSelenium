c8d0204485370a936013c8524669d92977d1829f
Adriano Bianchi Crosoletto <adrianobianchi@outlook.com>
2023-07-11 16:57:37 -0300

    method to wait for the element to appear

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,7 +1,7 @@
 #To optimize basic commands
 import conftest
-
-
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 class BasePage:
     def __init__(self):
         self.driver = conftest.driver
@@ -22,6 +22,13 @@ class BasePage:
     def check_element_exists(self,locator):
         assert self.element_find(locator).is_displayed(), f"Element '{locator}' not found on canvas."
 
-    #method that returns the text of the element
+    #method that returns the text await of the element appear
     def get_text_element(self,locator):
+        self.wait_element_appear(locator)
         return self.element_find(locator).text
+
+
+    #method to wait for the element to appear
+    def wait_element_appear(self, locator, timeout=60):
+        return WebDriverWait(self.driver,timeout).until(EC.presence_of_element_located(*locator))
+
