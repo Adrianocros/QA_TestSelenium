e1a4032d6be126532fc40edefecab231e9e6890b
Adriano Bianchi Crosoletto <adrianobianchi@outlook.com>
2023-07-11 14:11:09 -0300

    Refactoring the methods of CT01 add item to cart part 01

--- pages/home_page.py
+++ pages/home_page.py
@@ -8,8 +8,16 @@ class HomePage(BasePage):
     def __init__(self):
         self.driver = conftest.driver
         self.title_page = (By.XPATH,"//span[@class='title']")
+        self.item_inventory = (By.XPATH, "//*[@class='inventory_item_name' and text()='{}']")
+        self.add_to_cart = (By.XPATH, "//*[text()='Add to cart']")
+
 
     def verif_successfull_login(self):
         self.check_element_exists(self.title_page)
 
 
+    # Add products to cart
+    def add_item_cart(self,name_item):
+        item = (self.item_inventory[0],self.item_inventory[1].format(name_item))
+        self.click(item)
+        self.click(self.add_to_cart)
--- tests/test_ct01_add_product_cart.py
+++ tests/test_ct01_add_product_cart.py
@@ -1,6 +1,9 @@
+import time
+
 import pytest
 from selenium.webdriver.common.by import By
 import conftest
+from pages.home_page import HomePage
 from pages.login_page import LoginPage
 
 
@@ -10,56 +13,66 @@ class TestCT01:
     def test_ct01_add_product_cart(self):
         driver = conftest.driver
         login_page = LoginPage()
+        home_page = HomePage()
 
         #Login
         login_page.todo_login("standard_user","secret_sauce")
 
-          #Add products from cart
-        driver.find_element(By.XPATH,"//*[@class='inventory_item_name' and text()='Sauce Labs Backpack']").click()
-        driver.find_element(By.XPATH,"//*[text()='Add to cart']").click()
 
-        #Click from shopping cart
-        driver.find_element(By.XPATH,"//*[@class='shopping_cart_link']").click()
-        #Verifica se o item esta correto no carrinho
-        assert driver.find_element(By.XPATH,"//*[@class='inventory_item_name' and text()='Sauce Labs Backpack']").is_displayed()
+        #Add products from cart from home page
+        home_page.add_item_cart("Sauce Labs Backpack")
 
-        #Back to home screen
-        driver.find_element(By.XPATH,"//*[@id='continue-shopping']").click()
-
-        #Add another item to cart
-        driver.find_element(By.XPATH,"//*[@class='inventory_item_name' and text()='Test.allTheThings() T-Shirt (Red)']").click()
-        driver.find_element(By.XPATH,"//*[text()='Add to cart']").click()
-
-        #Clicking on cart
-        driver.find_element(By.XPATH,"//*[@class='shopping_cart_link']").click()
-
-        #Validating the cart
-        assert driver.find_element(By.XPATH,"//*[@class='inventory_item_name' and text()='Sauce Labs Backpack']").is_displayed()
-        assert driver.find_element(By.XPATH,"//*[@class='inventory_item_name' and text()='Test.allTheThings() T-Shirt (Red)']").is_displayed()
-
-        #Clickind on checkout
-        driver.find_element(By.XPATH,"//*[@id='checkout']").click()
-        assert driver.find_element(By.XPATH,"//span[@class='title']").is_displayed()
-
-
-        #fill in the fields
-        driver.find_element(By.XPATH,"//*[@id='first-name']").send_keys("Adriano")
-
-
-        driver.find_element(By.XPATH,"//*[@id='last-name']").send_keys("Bianchi")
-
-
-        driver.find_element(By.XPATH,"//*[@id='postal-code']").send_keys("07097420")
-
-
-        driver.find_element(By.XPATH,"//*[@id='continue']").click()
-
-
-        assert driver.find_element(By.XPATH,"//*[@class='inventory_item_name' and text()='Sauce Labs Backpack']").is_displayed()
-
-        #clicking finish
-        driver.find_element(By.XPATH,"//*[@id='finish']").click()
-
-        #Checks if the order has been completed
-        assert driver.find_element(By.XPATH,"//h2[@class='complete-header' and text()='Thank you for your order!']").is_displayed()
-        driver.find_element(By.XPATH,"//*[@class='btn btn_primary btn_small']").click()
+        # #Click from shopping cart
+        # driver.find_element(By.XPATH,"//*[@class='shopping_cart_link']").click()
+        #
+        # #Verifica se o item esta correto no carrinho
+        # assert driver.find_element(By.XPATH,"//*[@class='inventory_item_name' and text()='Sauce Labs Backpack']").is_displayed()
+        #
+        #
+        #
+        # #Back to home screen
+        # driver.find_element(By.XPATH,"//*[@id='continue-shopping']").click()
+        #
+        #
+        #
+        # #Add another item to cart
+        # driver.find_element(By.XPATH,"//*[@class='inventory_item_name' and text()='Test.allTheThings() T-Shirt (Red)']").click()
+        # driver.find_element(By.XPATH,"//*[text()='Add to cart']").click()
+        #
+        #
+        #
+        # #Clicking on cart
+        # driver.find_element(By.XPATH,"//*[@class='shopping_cart_link']").click()
+        #
+        #
+        #
+        # #Validating the cart
+        # assert driver.find_element(By.XPATH,"//*[@class='inventory_item_name' and text()='Sauce Labs Backpack']").is_displayed()
+        # assert driver.find_element(By.XPATH,"//*[@class='inventory_item_name' and text()='Test.allTheThings() T-Shirt (Red)']").is_displayed()
+        #
+        # #Clickind on checkout
+        # driver.find_element(By.XPATH,"//*[@id='checkout']").click()
+        # assert driver.find_element(By.XPATH,"//span[@class='title']").is_displayed()
+        #
+        #
+        # #fill in the fields
+        # driver.find_element(By.XPATH,"//*[@id='first-name']").send_keys("Adriano")
+        #
+        #
+        # driver.find_element(By.XPATH,"//*[@id='last-name']").send_keys("Bianchi")
+        #
+        #
+        # driver.find_element(By.XPATH,"//*[@id='postal-code']").send_keys("07097420")
+        #
+        #
+        # driver.find_element(By.XPATH,"//*[@id='continue']").click()
+        #
+        #
+        # assert driver.find_element(By.XPATH,"//*[@class='inventory_item_name' and text()='Sauce Labs Backpack']").is_displayed()
+        #
+        # #clicking finish
+        # driver.find_element(By.XPATH,"//*[@id='finish']").click()
+        #
+        # #Checks if the order has been completed
+        # assert driver.find_element(By.XPATH,"//h2[@class='complete-header' and text()='Thank you for your order!']").is_displayed()
+        # driver.find_element(By.XPATH,"//*[@class='btn btn_primary btn_small']").click()
